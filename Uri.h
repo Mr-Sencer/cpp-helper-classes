d52417126618c7a702dc6fd27ba319c3a315b20c
Mr-Sencer <husencerdiba@gmail.com>
2020-11-11 21:38:57 +0300

    Update Uri.h

--- Uri.h
+++ Uri.h
@@ -1,212 +1,229 @@
-// WindowsProject3.cpp : Uygulamanın giriş noktasını tanımlar.
-//
+#pragma once
 
-#include "framework.h"
+#include <map>
 
-#include "WindowsProject3.h"
+#include <string>
 
-#include "Environment.h"
+using namespace std;
 
-#include <time.h>
+class Uri {
+  protected: bool isWhiteAllSpace(char c) {
+    return c == 0x20 || c == 0x09 || c == 0x0a || c == 0x0b || c == 0xc || c == 0x0d;
 
-#include <d2d1_1.h>
-
-#define MAX_LOADSTRING 100
-Graphics * graphics;
-// Genel Değişkenler:
-HINSTANCE hInst;
-// geçerli örnek 
-#define chms 'text';
-#define Max_Size 10;
-
-WCHAR szTitle[MAX_LOADSTRING]; // Başlık çubuğu metni
-WCHAR szWindowClass[MAX_LOADSTRING]; // ana pencere sınıfı adı
-
-// Bu kod modülündeki işlevlerin bildirimlerini ilet:
-ATOM MyRegisterClass(HINSTANCE hInstance);
-BOOL InitInstance(HINSTANCE, int);
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-  _In_opt_ HINSTANCE hPrevInstance,
-  _In_ LPWSTR lpCmdLine,
-  _In_ int nCmdShow) {
-  UNREFERENCED_PARAMETER(hPrevInstance);
-  UNREFERENCED_PARAMETER(lpCmdLine);
-  Environment::Init();
-  // TODO: Kodu buraya yerleştirin.
-
-  // Genel dizeleri başlat
-  LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-  LoadStringW(hInstance, IDC_WINDOWSPROJECT3, szWindowClass, MAX_LOADSTRING);
-  MyRegisterClass(hInstance);
-
-  // Uygulama başlatması gerçekleştir:
-
-  if (!InitInstance(hInstance, nCmdShow)) {
-    return FALSE;
   }
+  bool isNumber(char c) {
+    return c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9';
+  }
+  public: map < string, string > Parameters;
+  string connectionMode;
+  enum Mode {
+    ConnectionMode,
+    Host,
+    Path,
+    Query,
+    Fragment
+  };
+  Mode mode = ConnectionMode;
+  string host;
+  string port;
+  int Port;
+  string AbsolutePath;
+  string OriginalString;
+  string QueryString;
+  string PathAndQuery;
+  string UserName;
+  string _Fragment;
+  string Password;
+  enum HostType {
+    IpAdress,
+    Domain,
+    None
+  };
+  HostType htype = None;
+  // mode = 0 = http https
 
-  HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT3));
+  void parseHost(string hoststr) {
+    int number = 0;
+    int len = hoststr.length();
+    bool is_host = false;
+    // 0: host 
+    //1:userName: 
+    //2:Password
+    //3 Port 
 
-  MSG msg;
+    int _mode = 0;
+    string buf;
 
-  // Ana ileti döngüsü:
-  while (GetMessage( & msg, nullptr, 0, 0)) {
-    if (!TranslateAccelerator(msg.hwnd, hAccelTable, & msg)) {
-      TranslateMessage( & msg);
-      DispatchMessage( & msg);
+    int point_number = 0;
+    for (int i = 0; i < len; i++) {
+
+      //192.168.1.4
+      if (hoststr[i] == '.') {
+        is_host = true;
+        point_number++;
+        if (point_number > 1) {
+          if (isNumber(hoststr[i - 1])) {
+            htype = IpAdress;
+          }
+
+        } else {
+          htype = Domain;
+        }
+
+      }
+      if (hoststr[i] == ':') {
+        if (is_host == true) {
+          host = buf;
+          buf = "";
+        }
+        if (isNumber(hoststr[i + 1]) == true && htype != None) {
+
+          for (int n = i + 1; n < len; n++) {
+            if (isNumber(hoststr[n]) == true) {
+              port += hoststr[n];
+
+            } else {
+              i = n - 1;
+
+              break;
+
+            }
+          }
+          Port = atoi(port.c_str());
+          continue;
+        }
+        number++;
+        point_number = 0;
+        is_host = false;
+      }
+      if (hoststr[i] == '@') {
+
+        string query1 = buf;
+        string buf1;
+        int length = query1.length();
+        int __mode = 0;
+        for (int i = 0; i < length; i++) {
+          if (query1[i] == ':') {
+            UserName = buf1;
+            __mode = 1;
+            buf1 = "";
+            continue;
+          }
+
+          buf1 += query1[i];
+          if (i == length - 1) {
+            if (__mode == 1) {
+              Password = buf1;
+              buf1 = "";
+
+            }
+
+          }
+
+        }
+        buf = "";
+        continue;
+      }
+      if (isWhiteAllSpace(hoststr[i]) == false) {
+        buf += hoststr[i];
+
+      }
+
+    }
+    if (htype != None && htype == Domain) {
+      host = buf;
     }
   }
+  void parseQuery(string query) {
+    int len = query.length();
+    string name = "";
+    string buf;
+    for (int i = 1; i < len; i++) {
+      if (query[i] == '=') {
+        name = buf;
+        buf = "";
+        continue;
+      }
+      if (query[i] == '&' || i == len - 1) {
+        Parameters.insert(pair < string, string > (name, buf));
+        buf = "";
+        name = "";
+        continue;
+      }
+      buf += query[i];
 
-  return (int) msg.wParam;
-}
-
-//
-//  İŞLEV: MyRegisterClass()
-//
-//  AMAÇ: Pencere sınıfını kaydeder.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance) {
-  WNDCLASSEXW wcex;
-
-  wcex.cbSize = sizeof(WNDCLASSEX);
-
-  wcex.style = CS_HREDRAW | CS_VREDRAW;
-  wcex.lpfnWndProc = WndProc;
-  wcex.cbClsExtra = 0;
-  wcex.cbWndExtra = 0;
-  wcex.hInstance = hInstance;
-  wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT3));
-  wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-  wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WINDOWSPROJECT3);
-  wcex.lpszClassName = szWindowClass;
-  wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-  return RegisterClassExW( & wcex);
-}
-
-//
-//   İŞLEV: InitInstance(HINSTANCE, int)
-//
-//   AMAÇ: Örnek tanıtıcısını kaydeder ve ana pencereyi oluşturur
-//
-//   AÇIKLAMALAR:
-//
-//        Bu işlevde, örnek tanıtıcısını genel değişkene kaydederiz ve
-//        ana program penceresini oluşturur ve görüntüleriz.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
-  hInst = hInstance; // Örnek tanıtıcısını genel değişkenimizde depola
-
-  HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-    CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-  if (!hWnd) {
-    return FALSE;
-  }
-
-  int a = 100;
-  * & a = 200;
-  DomParser * parser = new DomParser();
-  String _str;
-  _str += "ameno morasi binaero";
-  graphics = new Graphics;
-  graphics -> Init(hWnd);
-  std::string mystr;
-  mystr = _str.c_str();
-  std::string str = "<html> <a deneme='dfdfdfdffdfdfd'></a>  </html>";
-  time_t start;
-  time_t end;
-
-  time( & start);
-  for (int i = 0; i < 496; i++) {
-    _str += 'a';
-  }
-  time( & end);
-  time_t second = end - start;
-
-  time( & start);
-  for (int i = 0; i < 496; i++) {
-    mystr += 'a';
-  }
-
-  time( & end);
-  time_t newsecond = end - start;
-  if (newsecond > second) {
-
-  }
-  const char * chr = _str.c_str();
-
-  parser -> Parse(str);
-  Uri uri("https://www.youtube.com/watch?v=f0HiX_n0Fng");
-  ShowWindow(hWnd, nCmdShow);
-  UpdateWindow(hWnd);
-
-  return TRUE;
-}
-
-//
-//  İŞLEV: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  AMAÇ: Ana pencere için iletileri işler.
-//
-//  WM_COMMAND  - uygulama menüsünü işle
-//  WM_PAINT    - Ana pencereyi boya
-//  WM_DESTROY  - bir çıkış iletisi gönder ve dön
-//
-//
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-  switch (message) {
-  case WM_COMMAND: {
-    int wmId = LOWORD(wParam);
-    // Menü seçimlerini ayrıştır:
-    switch (wmId) {
-    case IDM_ABOUT:
-      DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-      break;
-    case IDM_EXIT:
-      DestroyWindow(hWnd);
-      break;
-    default:
-      return DefWindowProc(hWnd, message, wParam, lParam);
     }
   }
-  break;
-  case WM_PAINT: {
-    graphics -> BeginDraw();
-
-    // TODO: Buraya hdc kullanan herhangi bir çizim kodu ekleyin...
-    // EndPaint(hWnd, &ps);
-    graphics -> EndDraw();
-
+  Uri(string url) {
+    Parse(url);
   }
-  break;
-  case WM_DESTROY:
-    PostQuitMessage(0);
-    break;
-  default:
-    return DefWindowProc(hWnd, message, wParam, lParam);
-  }
-  return 0;
-}
+  public: void Parse(string url) {
+    int len = url.length();
+    OriginalString = url;
+    string buf;
 
-// Hakkında kutusu için ileti işleyicisi.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
-  UNREFERENCED_PARAMETER(lParam);
-  switch (message) {
-  case WM_INITDIALOG:
-    return (INT_PTR) TRUE;
+    int n = 0;
 
-  case WM_COMMAND:
-    if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
-      EndDialog(hDlg, LOWORD(wParam));
-      return (INT_PTR) TRUE;
+    for (int i = 0; i < len; i++) {
+      if (url[i] == ':' && url[i + 1] == '/' && url[i + 2] == '/') {
+        connectionMode = buf;
+        buf = "";
+        mode = Host;
+        i = i + 2;
+        continue;
+
+      }
+
+      if (url[i] == '/') {
+        if (n == 0) {
+          parseHost(buf);
+          mode = Path;
+          buf = "";
+          n++;
+          continue;
+
+        }
+        n++;
+
+      }
+      if (url[i] == '?') {
+        AbsolutePath += '/';
+        AbsolutePath += buf;
+        QueryString += '?';
+        buf += url[i];
+        mode = Query;
+        continue;
+      }
+      if (url[i] == '#') {
+        if (mode == Query) {
+          PathAndQuery += '/';
+          PathAndQuery += buf;
+
+          parseQuery(QueryString);
+        }
+        mode = Fragment;
+        buf += url[i];
+        continue;
+
+      }
+      if (isWhiteAllSpace(url[i]) == false) {
+        buf += url[i];
+        if (mode == Query) {
+          QueryString += url[i];
+
+        }
+        if (mode == Fragment) {
+          _Fragment += url[i];
+        }
+      }
+
+      if (i == len - 1) {
+        if (mode == Query) {
+          parseQuery(QueryString);
+        }
+      }
     }
-    break;
+
   }
-  return (INT_PTR) FALSE;
-}
+
+};
